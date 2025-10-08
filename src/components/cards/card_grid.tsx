94af90a114d02545725eca30104996b063f88af5
ri kankou <42364102+493249309@users.noreply.github.com>
2025-10-08 18:32:58 +0900

    Update card_grid.tsx

--- src/components/cards/card_grid.tsx
+++ src/components/cards/card_grid.tsx
@@ -59,7 +59,7 @@ const CardGrid: React.FC<CardGridProps> = ({ products }) => {
                                     alt={product.title}
                                     fill
                                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                    className="object-contain rounded-lg transition-all duration-300 hover:-translate-y-1 cursor-pointer"
+                                    className="object-contain rounded-lg transition-all duration-300 hover: cursor-pointer"
                                 />
                             </div>
                         </section>
