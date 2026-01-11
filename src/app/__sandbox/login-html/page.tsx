40a8a24629b1252a14fc68f6a432cd6612019532
uncertainflame <asanorin671@protonmail.com>
2026-01-11 16:36:03 +0900

    Create page.tsx

---
+++ src/app/__sandbox/login-html/page.tsx
@@ -0,0 +1,12 @@
+export default function LoginHtmlPreview() {
+  return (
+    <iframe
+      src="/sandbox/login-html/index.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}
