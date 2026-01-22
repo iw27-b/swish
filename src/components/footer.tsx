9ee8dcd8805f13194e4c4b58dcfe2fa6cd6e9556
jason <130282530+ls2396@users.noreply.github.com>
2026-01-22 20:35:23 +0900

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,32 +6,33 @@ const Footer: React.FC = () => {
   return (
     <footer className="px-5 sm:px-10 py-12 sm:py-16 border-t border-gray-200 bg-white">
       <div className="max-w-7xl mx-auto">
-        <div className="flex flex-col sm:flex-row items-center justify-start gap-8">
+        <div className="flex flex-col sm:flex-row items-center justify-between gap-8">
           
+          <div className="flex flex-row items-center">
+            <div className="text-gray-400 text-[24px] sm:text-4xl font-bold uppercase opacity-20 mr-10">
+              SWISH
+            </div>
+
+            <div className="flex flex-col items-start gap-6">
+              <nav className="flex flex-col items-start gap-2 text-sm uppercase font-medium">
+                <Link href="/news" className="text-black hover:underline">
+                  <span className="text-black">最新情報</span>
+                </Link>
+                <span className="text-black cursor-pointer">チーム紹介</span>
+                <span className="text-black cursor-pointer">カート</span>
+              </nav>
+            </div>
+          </div>
+
           <Image
             src="/images/Group 18.png"
             alt="Icon"
             width={80}
             height={80}
-            className="text-black font-bold text-2xl sm:text-4xl uppercase"
+            className="object-contain"
           />
 
-          <div className="text-gray-400 text-[24px] sm:text-4xl font-bold uppercase opacity-20 mr-5">
-            SWISH
-          </div>
-
-          <div className="flex flex-col items-start gap-6">
-            <nav className="flex flex-col items-start gap-2 text-sm uppercase font-medium">
-              <Link href="/news" className="text-black">
-                <span className="text-black">最新情報</span>
-              </Link>
-              <span className="text-black">チーム紹介</span>
-              <span className="text-black">カート</span>
-            </nav>
-          </div>
-
         </div>
-        <div className="absolute bottom-4 right-8"></div>
       </div>
     </footer>
   );
