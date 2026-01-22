1935cbaa8ef45bd16fdc7abea7ae7bef893081dd
jason <130282530+ls2396@users.noreply.github.com>
2026-01-22 20:19:50 +0900

    footer reflesh

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,20 +6,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="px-5 sm:px-10 py-12 sm:py-16 border-t border-gray-200 bg-white">
       <div className="max-w-7xl mx-auto">
-        <div className="flex flex-col sm:flex-row items-center justify-between gap-8">
-          <div className="text-gray-400 text-6xl sm:text-8xl font-bold uppercase opacity-20">
-            SWISH
-          </div>
-          <div className="flex flex-col items-start gap-6 ml-[-4rem]">
-            <nav className="flex flex-col items-start gap-2 text-sm uppercase font-medium">
-              {/* TODO: add links to the footer */}
-              <Link href="/news" className="text-black">
-                <span className="text-black">最新情報</span>
-              </Link>
-              <span className="text-black">チーム紹介</span>
-              <span className="text-black">カート</span>
-            </nav>
-          </div>
+        <div className="flex flex-col sm:flex-row items-center justify-start gap-8">
+          
           <Image
             src="/images/Group 18.png"
             alt="Icon"
@@ -27,6 +15,21 @@ const Footer: React.FC = () => {
             height={80}
             className="text-black font-bold text-2xl sm:text-4xl uppercase"
           />
+
+          <div className="text-gray-400 text-[24px] sm:text-4xl font-bold uppercase opacity-20 mr-5">
+            SWISH
+          </div>
+
+          <div className="flex flex-col items-start gap-6">
+            <nav className="flex flex-col items-start gap-2 text-sm uppercase font-medium">
+              <Link href="/news" className="text-black">
+                <span className="text-black">最新情報</span>
+              </Link>
+              <span className="text-black">チーム紹介</span>
+              <span className="text-black">カート</span>
+            </nav>
+          </div>
+
         </div>
         <div className="absolute bottom-4 right-8"></div>
       </div>
